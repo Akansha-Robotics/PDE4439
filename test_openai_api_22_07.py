993a1d2c4cac919589b077c7099bf54c0a74a5a0
Akansha-Robotics <145280259+Akansha-Robotics@users.noreply.github.com>
2025-08-08 10:02:40 +0400

    Create test_openai_api_22_07.py

---
+++ test_openai_api_22_07.py
@@ -0,0 +1,25 @@
+# Library for requesting HTTP calls 
+import requests
+# The below code is just loading the chat interface but there is another code that has the logic and brain of system 
+# so we can create HTTP calls, we are communicating with the code that has the logic to provide response 
+
+
+# Creates infinite loop to keep the conversation ongoing
+while True:
+    # Collects user input from terminal and stores in variable q (question) 
+    q = input("You: ")
+
+    # If q variable is empty by user clicking enter
+    if not q:
+        # end the infinite loop
+        break
+    # if value exist in q 
+
+    # Converts the user input from q variable in JSON as the triggers are in that format 
+    # POST request is sent to chat bot API for processing 
+    res = requests.post("http://127.0.0.1:8000/analyze", json={"user_input": q})
+    
+    # Learning Point: ALWAYS check the IP link matches when you start the server 
+
+    # Static string of Bot to be displayed along with response from chat bot api to be printed
+    print("Bot:", res.json())
